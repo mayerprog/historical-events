0a59e300ad299802cedd815ce9d0655f43815de1
mayerprog <supermayerehs@gmail.com>
2024-11-18 22:14:46 +0300

    feat: configure working with data + add first functions and conditionals

--- src/components/HistoryCircle/HistoryCircle.tsx
+++ src/components/HistoryCircle/HistoryCircle.tsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HistoryCircle.module.scss";
+import events from "./../../../mock-data.json";
+
+interface Area {
+  area: string;
+  years: Year[];
+}
+
+interface Year {
+  year: string;
+  text: string;
+}
 
 const HistoryCircle: React.FC = () => {
-  const smallCircles = Array.from({ length: 6 });
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const chosenYears = events[activeIndex].years;
+
+  const rotateDegree = (index: number) => index * (360 / events.length) + 30;
+
+  const handleEventClick = (index: number) => {
+    setActiveIndex(index);
+  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Исторические даты</h1>
       <div className={styles.circleContainer}>
         <div className={styles.circle}>
-          <span className={`${styles.date} ${styles.left}`}>2015</span>
-          <span className={`${styles.date} ${styles.right}`}>2022</span>
-          {smallCircles.map((_, index) => (
+          <span className={`${styles.date} ${styles.left}`}>
+            {chosenYears[0]?.year}
+          </span>
+          <span className={`${styles.date} ${styles.right}`}>
+            {chosenYears[chosenYears.length - 1]?.year}
+          </span>
+          {events.map((_, index) => (
             <div
               key={index}
-              className={styles.smallCircle}
+              className={`${
+                index === activeIndex
+                  ? styles.openedCircle
+                  : styles.closedCircle
+              }`}
               style={{
-                transform: `rotate(${
-                  index * (360 / smallCircles.length) + 30
-                }deg) translate(0, -225px)`,
+                transform: `rotate(${rotateDegree(
+                  index
+                )}deg) translate(0, -225px)`,
               }}
-            ></div>
+              onClick={() => handleEventClick(index)}
+            >
+              {index === activeIndex && (
+                <span
+                  style={{
+                    transform: `rotate(-${rotateDegree(index)}deg)`,
+                  }}
+                  className={styles.circleNumber}
+                >
+                  {index + 1}
+                </span>
+              )}
+            </div>
           ))}
         </div>
       </div>
