b6d32ea92d91947b66076fc6f6e0e5c50df02d3d
mayerprog <supermayerehs@gmail.com>
2024-11-19 13:37:48 +0300

    feat: add numbers and small circles animations

--- src/components/HistoryCircle/HistoryCircle.tsx
+++ src/components/HistoryCircle/HistoryCircle.tsx
@@ -17,6 +17,9 @@ const HistoryCircle: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const circleRef = useRef<HTMLDivElement>(null);
 
+  const circleRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const circleNumberRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
   const chosenYears = events[activeIndex].years;
 
   const rotateDegree = (index: number) => index * (360 / events.length) + 30;
@@ -24,7 +27,7 @@ const HistoryCircle: React.FC = () => {
   const handleEventClick = (index: number) => {
     if (index === activeIndex) return;
     const rotationDiff = (activeIndex - index) * (360 / events.length);
-    const duration = Math.min(0.9, 1 / (Math.abs(rotationDiff) / 360));
+    const duration = Math.min(0.8, 1 / (Math.abs(rotationDiff) / 360));
 
     gsap.to(circleRef.current, {
       rotation: `+=${rotationDiff}`,
@@ -32,6 +35,46 @@ const HistoryCircle: React.FC = () => {
       ease: "power1.inOut",
       onComplete: () => setActiveIndex(index),
     });
+
+    const currentCircle = circleRefs.current[activeIndex];
+    const nextCircle = circleRefs.current[index];
+    const currentCircleNumber = circleNumberRefs.current[activeIndex];
+    const nextCircleNumber = circleNumberRefs.current[index];
+
+    if (currentCircle) {
+      gsap.to(currentCircle, {
+        width: 8,
+        height: 8,
+        backgroundColor: "#6A6A6A",
+        duration: 0.5,
+        ease: "power1.inOut",
+      });
+      if (currentCircleNumber) {
+        gsap.fromTo(
+          currentCircleNumber,
+          { opacity: 1, scale: 1 },
+          { opacity: 0, scale: 0.8, duration: 0.5, ease: "power1.inOut" }
+        );
+      }
+    }
+    if (nextCircle) {
+      gsap.to(nextCircle, {
+        width: 50,
+        height: 50,
+        border: "1px solid #ccc",
+        backgroundColor: "#f9f9f9",
+        zIndex: "998",
+        duration: 0.5,
+        ease: "expo.inOut",
+      });
+      if (nextCircleNumber) {
+        gsap.fromTo(
+          nextCircleNumber,
+          { opacity: 0, scale: 0.8 },
+          { opacity: 1, scale: 1, duration: 0.5, ease: "power1.inOut" }
+        );
+      }
+    }
   };
 
   return (
@@ -53,10 +96,16 @@ const HistoryCircle: React.FC = () => {
                 )}deg) translate(0, -225px)`,
               }}
               onClick={() => handleEventClick(index)}
+              ref={(el) => (circleRefs.current[index] = el)}
             >
-              {index === activeIndex && (
-                <span className={styles.circleNumber}>{index + 1}</span>
-              )}
+              <span
+                className={`${styles.circleNumber} ${
+                  index === activeIndex ? styles.active : styles.hidden
+                }`}
+                ref={(el) => (circleNumberRefs.current[index] = el)}
+              >
+                {index + 1}
+              </span>
             </div>
           ))}
         </div>
